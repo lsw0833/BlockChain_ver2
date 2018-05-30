526164589cf1336a87c3264a38ef1dbd25f9644b
Sangwonlee93 <lsw0833@hanmail.net>
2018-05-30 14:44:43 +0900

    Add no data case 2

--- CreateBlock/block/MakeBlock.py
+++ CreateBlock/block/MakeBlock.py
@@ -5,27 +5,26 @@ import sys
 import os
 
 if __name__ == "__main__":
-    version =1
-    data = []
-    bits = 440711666
-
-    print(Miner.createBlock(version,"0000000000000000000000000000000000000000000000000000000000000000",data,bits,4).printBlockInfo())
-    #li = []
-    #start = 0;
-    #last = 0;
-    #for i in range(0,len(os.environ['datainMining'])):
-    #   if os.environ['datainMining'][i] ==",":
-    #       last = i
-    #       li.append(os.environ['datainMining'][start:last])
-    #       start = last+1
-    #   elif i == len(os.environ['datainMining']) -1:
-    #       last = i+1
-    #       li.append(os.environ['datainMining'][start:last])
-    #block=Miner.createBlock(int(os.environ['version']), os.environ['previous'], li, int(os.environ['bits']), int(os.environ['zero']))
-    #print(block.nonce)
-    #print(block.merkleHash)
-    #print(block.version)
-    #print(block.blockHash)
-    #print(block.time)
-    #print(block.previousBlockHash)
-    #print(block.bits)
+    #version =1
+    #data = ["no data"]
+    #bits = 440711666
+    #print(Miner.createBlock(version,"0000000000000000000000000000000000000000000000000000000000000000",data,bits,4).printBlockInfo())
+    li = []
+    start = 0;
+    last = 0;
+    for i in range(0,len(os.environ['datainMining'])):
+       if os.environ['datainMining'][i] ==",":
+           last = i
+           li.append(os.environ['datainMining'][start:last])
+           start = last+1
+       elif i == len(os.environ['datainMining']) -1:
+           last = i+1
+           li.append(os.environ['datainMining'][start:last])
+    block=Miner.createBlock(int(os.environ['version']), os.environ['previous'], li, int(os.environ['bits']), int(os.environ['zero']))
+    print(block.nonce)
+    print(block.merkleHash)
+    print(block.version)
+    print(block.blockHash)
+    print(block.time)
+    print(block.previousBlockHash)
+    print(block.bits)
